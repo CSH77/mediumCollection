4efb7bde973f82c77fbc992df53d047d3dc62e37
SeungHwan <seunghwanchoi@outlook.com>
2019-07-12 16:11:37 -0600

    3sum inital commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-    
+
+//Given an array nums of n integers, are there elements a, b, c in nums
+//such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+/*
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+ */
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+
+
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+
     return 0;
 }
-
-
-
