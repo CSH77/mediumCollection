5c6925869a84dd5f51cb591f0e9e00c33bce65b7
SeungHwan <seunghwanchoi@outlook.com>
2019-07-23 23:15:13 -0600

    recursive method

--- main.cpp
+++ main.cpp
@@ -4,12 +4,66 @@
 
 using namespace std;
 
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+Example:
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> numList;
+
+    void iter(TreeNode* node)
+    {
+        if(node == NULL)
+            return;
+
+        iter(node->left);
+        // cout << node->val;
+        numList.push_back(node->val);
+        iter(node->right);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        iter(root);
+
+        return numList;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    TreeNode* node1 = new TreeNode(1);
+    TreeNode* node2 = new TreeNode(2);
+    TreeNode* node3 = new TreeNode(3);
+    node1->right = node2;
+    node2->left = node3;
+
+    Solution obj;
+    vector<int> answer = obj.inorderTraversal(node1);
+
+    for(int n : answer)
+    {
+        cout << n << " ";
+    }
+    cout << endl;
+
 
     return 0;
 }
-
-
-
