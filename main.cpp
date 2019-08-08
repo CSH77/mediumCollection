c44781bf5747347888b529b5b40a8c67ba874fe4
SeungHwan <seunghwanchoi@outlook.com>
2019-08-07 22:52:21 -0600

    working solution..

--- main.cpp
+++ main.cpp
@@ -20,31 +20,29 @@ Output:
 
 class Solution {
 public:
-    vector<vector<int>> answer;
-
-    void subsetHelper(vector<int>& nums, vector<int> collection, int left, int right)
+    vector<vector<int>> subsets(vector<int>& nums)
     {
-        if(left > right)
-            return;
+        vector<vector<int>> vv;
+        vector<int> v;
 
-        for(int n : collection)
-            cout << n << " ";
-        cout << endl;
+        if (nums.size() == 0)
+            return vv;
 
+        PowerSet(vv, v, nums, 0);
 
-        for(int i = left; i < right; i++)
-        {
-            // vector<int> temp(collection.begin(), collection.end());
-            vector<int> temp = collection;
-            temp.push_back(collection[i]);
-            subsetHelper(nums, temp, i + 1, right);
-        }
+        return vv;
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        subsetHelper(nums, vector<int>(), 0, nums.size());
+    void PowerSet(vector<vector<int>>& vv, vector<int>& v,vector<int>& nums, int begin) {
 
-        return answer;
+        vv.push_back(v);
+
+        for(int i = begin; i < nums.size(); i++)
+        {
+            v.push_back(nums[i]);
+            PowerSet(vv, v, nums, i+1);
+            v.pop_back();
+        }
     }
 };
 
@@ -53,7 +51,14 @@ int main()
     vector<int> input = {1,2,3};
     Solution obj;
 
-    obj.subsets(input);
+    vector<vector<int>> answer = obj.subsets(input);
+
+    for(vector<int> v : answer)
+    {
+        for(int n : v)
+            cout <<  n << " ";
+        cout << endl;
+    }
 
 
     return 0;
