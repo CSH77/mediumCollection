1ca14671e256aee2525d5cee41436ad7457baa4e
SeungHwan <seunghwanchoi@outlook.com>
2019-08-11 11:49:12 -0600

    not yet done

--- main.cpp
+++ main.cpp
@@ -4,9 +4,41 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int min;
+    int max;
+
+    int search(vector<int>& nums, int l, int r, int target)
+    {
+        if(l == r)
+            return l;
+
+        int mid = (l + r) / 2;
+        if(nums[mid] == target)
+        {
+            if(nums[l] == target)
+                min = l;
+
+            if(nums[r] == target)
+                max = r;
+
+            search(nums, l, mid - 1);
+        }
+
+
+
+
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<int> input = {5,7,7,8,8,10};
 
     return 0;
 }
