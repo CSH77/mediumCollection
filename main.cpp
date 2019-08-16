fffb5b1f195aeea0a7527e17cc54092207266be3
SeungHwan <seunghwanchoi@outlook.com>
2019-08-15 21:30:47 -0600

    using set, remove single element which is bigger than current one when add any element, thus make number order as increasing order

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <set>
 #include <vector>
-
 using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS( vector<int> &nums ) {
+        set<int> lis;
+        for ( auto num : nums )
+        {
+            if( lis.count( num ) )
+                continue;
+
+            lis.insert( num );
+            auto it = lis.upper_bound( num );
+            if( it != lis.end() )
+                lis.erase( it );
+        }
+        return lis.size();
+    }
+};
 
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<int> input = {10,9,2,5,3,7,101,18}; //expect 4   {2,3,7,101}
+    // vector<int> input = {3,4,-1, 0, 6, 2, 3}; //expect  4   {-1, 0, 2, 3}
+
+    Solution obj;
+    cout << obj.lengthOfLIS(input);
+
 
     return 0;
 }
