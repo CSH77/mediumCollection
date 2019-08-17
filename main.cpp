08781c0bc4f3f15a9bbc2c7d0eaf9ec37e4cab60
SeungHwan <seunghwanchoi@outlook.com>
2019-08-16 21:23:12 -0600

    just for loop..!

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <set>
 #include <vector>
-
 using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+
+        int total = 0;
+        size_t next = 1;
+        for(int i = s.size() - 1; i >= 0; i--)
+        {
+            int cur = (s[i] - 'A') + 1;
+            total += cur * next;
+            next = next * ('Z' - 'A' + 1);
+        }
+        return total;
+    }
+};
+
 
 int main()
 {
-    cout << "hello world!" << endl;
+    Solution obj;
+    cout << obj.titleToNumber("A");
 
     return 0;
 }
