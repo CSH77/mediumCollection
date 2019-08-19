3aa75a346c71e00bc63fc9f52031a3e1da84aedb
SeungHwan <seunghwanchoi@outlook.com>
2019-08-19 16:49:06 -0600

    use binary search logic to find answer.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int l = 1, r = x;
+        while(l <= r)
+        {
+            long long int mid = l + (r - l) / 2;
+            if(mid * mid == x)
+                return mid;
+            else if(mid * mid < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return l - 1;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    Solution obj;
+    cout << obj.mySqrt(8); //expect 2;
 
     return 0;
 }
