a75e4833869d9bba6f99ed8bdc8d90be9a501eb4
SeungHwan <seunghwanchoi@outlook.com>
2019-08-19 22:26:56 -0600

    need to limit numer to 32 bits.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,26 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int getSum(int a, int b) {
+
+        long long carry; //64 bit
+        while( b != 0 )
+        {
+            carry = a & b;
+            a = a ^ b;
+            b = (carry & 0xffffffff) << 1;
+
+        }
+        return a;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    Solution obj;
+    cout << obj.getSum(7,5);
 
     return 0;
 }
