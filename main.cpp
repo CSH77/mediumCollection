ff3f2ead737894f9108c37ef5ad974aa2a526c81
SeungHwan <seunghwanchoi@outlook.com>
2019-12-27 11:44:35 -0700

    solution 2

--- main.cpp
+++ main.cpp
@@ -6,44 +6,35 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        // set<char> sset;
-        int count = 0;
-        int maxCount = 0;
+    int lengthOfLongestSubstring(string s)
+    {
+        set<char> myset;
+        int n = s.length();
+        int ans = 0, i = 0, j = 0;
 
-        for(int i = 0; i < s.size(); i++)
+        while(i < n && j < n)
         {
-            set<char> sset;
-            int curIndex = i;
-            count = 0;
-            while( curIndex < s.size())
+            auto search = myset.find(s.at(j));
+            if(search == myset.end())
             {
-                auto search = sset.find(s[curIndex]);
-                if(search != sset.end()) // repeated char
-                {
-                    break;
-                }
-                else
-                {
-                    sset.insert(s[curIndex]);
-                    count++;
-                }
-
-                if(count > maxCount)
-                    maxCount = count;
-
-                curIndex++;
+                myset.insert(s.at(j++));
+                ans = max(ans, j - i);
+            }
+            else
+            {
+                myset.erase(s.at(i++));
             }
         }
-        return maxCount;
-    }
-};
 
+        return ans;
+    }
+
+};
 int main()
 {
     //string input = "pwwkew"; //expect The answer is "wke", with the length of 3.
-    string input = "abcabcbb";//  The answer is "abc", with the length of 3.
-    // string input = "dvdf"; //expect 3
+    //string input = "abcabcbb";//  The answer is "abc", with the length of 3.
+    string input = "dvdf"; //expect 3
     Solution obj;
     cout << obj.lengthOfLongestSubstring(input);
 
