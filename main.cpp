2d0befd8808fb8fabfc960894f844477407c7683
SeungHwan <seunghwanchoi@outlook.com>
2020-02-05 21:03:52 -0700

    simple inorder iterative method

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <stack>
 using namespace std;
 
-/*
-    Input: root = [2,1,3], p = 1
-    Output: 2
-    Explanation: 1's in-order successor node is 2. Note that both p and the return value is of TreeNode type.
- */
 
 struct TreeNode {
     int val;
@@ -19,39 +15,65 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        TreeNode* res = nullptr;
+        //use iterative method,
 
-        auto cur = root;
-        while (cur)
+        stack<TreeNode*> stk;
+        TreeNode* node = root;
+        bool flag = false;
+
+        while(!stk.empty()|| node != NULL)
         {
-            if(cur->val > p->val)
+            while(node != NULL)
             {
-                res=cur;
-                cur=cur->left;
+                stk.push(node);
+                node = node->left;
             }
-            else
-            {
-                cur = cur->right;
-            }
-        }
 
-        return res;
+            //node is NULL at this point,
+            node = stk.top();
+            stk.pop();
+            if(flag)
+                return node;
+
+            if(node == p)
+                flag = true;
+            // if(flag)
+            //     cout << node->val << " ";
+            node = node->right;
+        }
+        return node;
     }
 };
 
 int main()
 {
-    TreeNode* node2 = new TreeNode(2);
-    TreeNode* node1 = new TreeNode(1);
-    TreeNode* node3 = new TreeNode(3);
+    // TreeNode* node2 = new TreeNode(2);
+    // TreeNode* node1 = new TreeNode(1);
+    // TreeNode* node3 = new TreeNode(3);
+    // node2->left = node1;
+    // node2->right = node3;
 
+    TreeNode* node5 = new TreeNode(5);
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node6 = new TreeNode(6);
+    TreeNode* node2 = new TreeNode(2);
+    TreeNode* node4 = new TreeNode(4);
+    TreeNode* node1 = new TreeNode(1);
+    node5->left = node3;
+    node5->right = node6;
+    node3->left = node2;
+    node3->right = node4;
     node2->left = node1;
-    node2->right = node3;
+
 
     Solution obj;
-    TreeNode* answer = obj.inorderSuccessor(node2, node1 );
+    TreeNode* answer = obj.inorderSuccessor(node5, node6 ); //expect NULL
+    // TreeNode* answer = obj.inorderSuccessor(node5, node3 ); //expect node4
 
-    cout << answer->val << endl;
+    if(!answer)
+        cout << "NULL" << endl;
+    else
+        cout << answer->val << endl;
 
 
     return 0;
