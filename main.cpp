689f58906516e3f5a5d9f6b7478d87a3bcb61a98
SeungHwan <seunghwanchoi@outlook.com>
2020-02-15 21:11:39 -0700

    use XOR

--- main.cpp
+++ main.cpp
@@ -4,9 +4,24 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int answer = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            answer = answer ^ nums[i];
+        }
+        return answer;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<int> input = {4,1,2,1,2};
+
+    Solution obj;
+    cout << obj.singleNumber(input) << endl;
 
     return 0;
 }
