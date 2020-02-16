c3a838ac892d5ee8c0002b3b0e087beb0d1c549d
SeungHwan <seunghwanchoi@outlook.com>
2020-02-15 18:36:50 -0700

    split to two arrays and merge them into one leter

--- main.cpp
+++ main.cpp
@@ -4,9 +4,33 @@
 
 using namespace std;
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        auto ptr = nums.end() - k;
+        vector<int> tmp = vector<int>(ptr,nums.end());
+        for(int i = nums.size() - k - 1; i>=0; i--){
+            nums[i+k] = nums[i];
+        }
+        for(int i=0;i<k;i++){
+            nums[i] = tmp[i];
+        }
+    }
+};
+
+
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<int> nums = {1,2,3,4,5,6,7};
+
+    Solution obj;
+    obj.rotate(nums, 3); //expect 5,6,7,1,2,3,4 with 3
+
+
+    for(auto v : nums)
+        cout << v << " ";
+    cout << endl;
 
     return 0;
 }
