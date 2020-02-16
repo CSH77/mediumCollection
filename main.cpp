ff9c5a7d73e0bf1a81045d6165c2b75236c046e1
SeungHwan <seunghwanchoi@outlook.com>
2020-02-15 20:36:13 -0700

    use set to find duplicate

--- main.cpp
+++ main.cpp
@@ -4,9 +4,34 @@
 
 using namespace std;
 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> sset;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            auto search = sset.find(nums[i]);
+
+            if(search != sset.end()) //found duplicated number.
+            {
+                return true;
+            }
+            else
+                sset.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    // vector<int> input = {1,1,1,3,3,4,3,2,4,2};
+    vector<int> input = {1,2,3,4};
+
+
+    Solution obj;
+    cout << obj.containsDuplicate(input) << endl;
 
     return 0;
 }
