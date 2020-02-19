67265008788b95abf1595341b60587247c1d6d12
SeungHwan <seunghwanchoi@outlook.com>
2020-02-18 23:04:04 -0700

    used hashmap

--- main.cpp
+++ main.cpp
@@ -7,9 +7,38 @@
 
 using namespace std;
 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mmap; //value, index number
+        for(int i = 0; i < nums.size(); i++)
+            mmap.insert({nums[i], i});
+
+        //find target combination
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int numberNeeded = target - nums[i];
+
+            //find nessasary number, occurance must be more than one.
+            auto search = mmap.find(numberNeeded);
+            if(search != mmap.end() && search->second != i) //it can not be same value
+                return vector<int>{i, search->second};
+        }
+        return vector<int>{};
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    // vector<int> input = {2, 7, 11, 15};
+    vector<int> input = {3,2,4}; //expect 1,2
+    Solution obj;
+    vector<int> answer = obj.twoSum(input, 6);
+
+    for(auto v : answer)
+        cout << v << " ";
+    cout << endl;
 
     return 0;
 }
