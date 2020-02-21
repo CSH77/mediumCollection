be69e50218cc6505e81fdc1adc6e422b84696336
SeungHwan <seunghwanchoi@outlook.com>
2020-02-21 13:05:06 -0700

    reverse char from end

--- main.cpp
+++ main.cpp
@@ -7,12 +7,29 @@
 
 using namespace std;
 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        //get mid,
+        int mid = s.size() / 2;
+
+        for(int i = s.size() - 1; i >= mid; i--)
+        {
+            swap(s[i], s[s.size() - 1 - i]);
+        }
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<char> input = {'h','e','l','l','o'};
+    Solution obj;
+    obj.reverseString(input);
+
+    for(auto c : input)
+        cout << c << " ";
+    cout << endl;
 
     return 0;
 }
-
-
 
