649379c53bfa2ad601e9e496521f4b205eb2b02a
SeungHwan <seunghwanchoi@outlook.com>
2020-02-21 13:21:37 -0700

    use arr as hashmap

--- main.cpp
+++ main.cpp
@@ -4,12 +4,41 @@
 #include <map>
 #include <queue>
 #include <stack>
+#include <string>
 
 using namespace std;
 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+
+        int arr[255];
+        for(int i = 0; i < 255; i++)
+            arr[i] = 0;
+
+        for(int i = 0; i < s.size(); i++)
+            arr[s[i]] += 1;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            arr[t[i]] -= 1;
+            if(arr[t[i]] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    // string s = "anagram", t = "nagaram";
+    string s = "rat", t = "car";
+
+    Solution obj;
+    cout << obj.isAnagram(s, t) << endl;
 
     return 0;
 }
