3c199ea5a13c206cdaa2b5dccd8486df4266c28c
SeungHwan <seunghwanchoi@outlook.com>
2020-02-21 19:01:50 -0700

    find longerst prefix, sort by string size and start from shortest string length, compare each string one by one for entire string

--- main.cpp
+++ main.cpp
@@ -4,12 +4,64 @@
 #include <map>
 #include <queue>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+
+        if(strs.size() < 2)
+            return strs[0];
+
+        int numberOfString = strs.size();
+
+        //sort by length of string.
+        auto comp = [](string& a, string& b){
+            return a.size() < b.size();
+        };
+        sort(strs.begin(), strs.end(), comp);
+
+        string answer;
+
+        int stringIndex = 0;
+        for(int i = 0; i < strs[0].length(); i++)
+        {
+            char charToCompare = strs[0][i];
+            bool commonChar = true;
+            for(int j = 0; j < strs.size(); j++)
+            {
+                if(charToCompare != strs[j][i])
+                {
+                    commonChar = false;
+                    break;
+                }
+            }
+            if(commonChar)
+                answer.push_back(charToCompare);
+            else
+                break;
+        }
+        return answer;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    // vector<string> input = {"flower","flow","flight"};
+    // vector<string> input = {"a"};
+    // vector<string> input;
+    vector<string> input = {"aca","cba"};
+
+
+    // vector<string> input = {"dog","racecar","car"};
+    Solution obj;
+    string answer = obj.longestCommonPrefix(input);
+
+    cout << answer << endl;
 
     return 0;
 }
