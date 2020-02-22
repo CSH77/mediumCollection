dcb1512197901a10cd587b7f52cfe22c4819337d
SeungHwan <seunghwanchoi@outlook.com>
2020-02-22 00:22:19 -0700

    reverse linked list

--- main.cpp
+++ main.cpp
@@ -7,9 +7,55 @@
 
 using namespace std;
 
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+
+        while(cur)
+        {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    ListNode* head = new ListNode(0);
+    ListNode* node = head;
+    for(int i = 1; i <= 5; i++)
+    {
+        node->next = new ListNode(i);
+        node = node->next;
+    }
+
+    node = head;
+    node = node->next;
+
+    Solution obj;
+    ListNode* answer = obj.reverseList(node);
+
+    while(answer)
+    {
+        cout << answer->val << " ";
+        answer = answer->next;
+    }
+    cout << endl;
+
 
     return 0;
 }
