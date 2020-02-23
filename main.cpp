7109d444d81d3286887cea963813a6c301125db5
SeungHwan <seunghwanchoi@outlook.com>
2020-02-22 22:03:25 -0700

    check cycle by two pointers

--- main.cpp
+++ main.cpp
@@ -7,6 +7,39 @@
 
 using namespace std;
 
+
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next)
+            return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(slow && fast)
+        {
+            if(slow == fast)
+                return true;
+
+            slow = slow->next;
+
+            if(!fast->next) //jump twice
+                return false;
+            else
+                fast = fast->next->next;
+        }
+
+        return false;
+    }
+};
+
 int main()
 {
     cout << "hello world!" << endl;
