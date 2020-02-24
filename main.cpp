7c3e8feff05e706396282d5e3d1512918628c19f
SeungHwan <seunghwanchoi@outlook.com>
2020-02-23 17:54:03 -0700

    recursive method to get height of tree

--- main.cpp
+++ main.cpp
@@ -7,9 +7,42 @@
 
 using namespace std;
 
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return left > right ? left + 1 : right + 1;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node9 = new TreeNode(9);
+    TreeNode* node20 = new TreeNode(20);
+    TreeNode* node15 = new TreeNode(15);
+    TreeNode* node7 = new TreeNode(7);
+
+    node3->left = node9;
+    node3->right = node20;
+    node20->left = node15;
+    node20->right = node7;
+
+    Solution obj;
+    cout << obj.maxDepth(node3) << endl;
 
     return 0;
 }
