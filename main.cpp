9f5f4c020a78c82ea69a4bf0b11f34821bd6699d
SeungHwan <seunghwanchoi@outlook.com>
2020-02-24 23:32:40 -0700

    maximum subarray, dynamic programming

--- main.cpp
+++ main.cpp
@@ -4,15 +4,33 @@
 #include <map>
 #include <queue>
 #include <stack>
+#include <limits>
 
 using namespace std;
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = numeric_limits<int>::min();
+        int currentSum = numeric_limits<int>::min();
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            currentSum = max(currentSum, 0) + nums[i];
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    Solution obj;
+    vector<int> input = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << obj.maxSubArray(input) << endl;
 
     return 0;
 }
-
 
 
