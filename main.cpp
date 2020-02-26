31aaf24bc116c62f582c6e7d822c6b6cc5c265a1
SeungHwan <seunghwanchoi@outlook.com>
2020-02-26 01:02:11 -0700

    used stack to validate parentheses

--- main.cpp
+++ main.cpp
@@ -7,9 +7,72 @@
 
 using namespace std;
 
+class Solution {
+public:
+    bool isValid(string s) {
+
+        if(s.length() % 2 != 0)
+            return false;
+
+        stack<char> stk;
+        for(char c : s)
+        {
+            switch(c)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    stk.push(c);
+                    break;
+                case ')':
+                {
+                    if(stk.empty())
+                        return false;
+                    char comp = stk.top();
+                    stk.pop();
+                    if(comp != '(')
+                        return false;
+                    break;
+                }
+                case '}':
+                {
+                    if(stk.empty())
+                        return false;
+                    char comp = stk.top();
+                    stk.pop();
+                    if(comp != '{')
+                        return false;
+                    break;
+                }
+                case ']':
+                {
+                    if(stk.empty())
+                        return false;
+                    char comp = stk.top();
+                    stk.pop();
+                    if(comp != '[')
+                        return false;
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+        if(!stk.empty())
+            return false;
+
+        return true;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    // string input = "()[]{}"; //true
+    // string input = "([)]"; //false
+    string input = "("; //false
+
+    Solution obj;
+    cout << obj.isValid(input) << endl;
 
     return 0;
 }
