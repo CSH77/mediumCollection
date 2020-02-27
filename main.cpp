a3cdbf91687b740339445c70483d1d4bb3be436f
SeungHwan <seunghwanchoi@outlook.com>
2020-02-27 13:08:18 -0700

    descripted added

--- main.cpp
+++ main.cpp
@@ -3,18 +3,6 @@
 #include <set>
 using namespace std;
 
-/*
-You are given two non-empty linked lists representing two non-negative integers.
-The digits are stored in reverse order and each of their nodes contain a single digit.
-Add the two numbers and return it as a linked list.
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-Example:
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
- */
-
 struct ListNode {
     int val;
     ListNode *next;
@@ -25,68 +13,57 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
-        ListNode* temp = new ListNode(0);
-        ListNode* head = temp;
+        ListNode* head = new ListNode(0);
+        ListNode* node = head;
+
+        int sum = 0;
         int carryOver = 0;
-        int currentNumber = 0;
-        while(l1 != NULL && l2 != NULL)
+        while(l1 && l2)
         {
-            currentNumber = 0;
-            if( carryOver )
-            {
-                currentNumber = carryOver;
-            }
+            int sum = l1->val + l2->val + carryOver;
+            carryOver = sum / 10;
+            sum = sum % 10;
 
-            currentNumber += l1->val + l2->val;
-            carryOver = currentNumber / 10;
-            currentNumber = currentNumber % 10;
-
-            temp->next = new ListNode(currentNumber);
-            temp = temp->next;
+            node->next = new ListNode(sum);
+            node = node->next;
 
             l1 = l1->next;
             l2 = l2->next;
-
         }
 
-        while(l1 != NULL)
+        while(l1)
         {
-            currentNumber = 0;
-            if( carryOver )
-                currentNumber = carryOver;
+            sum = l1->val + carryOver;
+            carryOver = sum / 10;
+            sum = sum % 10;
+            node->next = new ListNode(sum);
+            node = node->next;
 
-            currentNumber += l1->val;
-            carryOver = currentNumber / 10;
-            currentNumber = currentNumber % 10;
-
-            temp->next = new ListNode(currentNumber);
-            temp = temp->next;
             l1 = l1->next;
         }
 
-        while(l2 != NULL)
+        while(l2)
         {
-            currentNumber = 0;
-            if( carryOver )
-                currentNumber = carryOver;
-
-            currentNumber += l2->val;
-            carryOver = currentNumber / 10;
-            currentNumber = currentNumber % 10;
-
-            temp->next = new ListNode(currentNumber);
-            temp = temp->next;
+            sum = l2->val + carryOver;
+            carryOver = sum / 10;
+            sum = sum % 10;
+            node->next = new ListNode(sum);
+            node = node->next;
             l2 = l2->next;
         }
 
         if(carryOver)
-            temp->next = new ListNode(carryOver);
+        {
+            node->next = new ListNode(carryOver);
+            node = node->next;
+        }
 
-        ListNode* del = head;
-        head = head->next;
-        delete del;
+        node = head;
+        ListNode* delNode = node;
+        node = node->next;
+        delete delNode;
 
-        return head;
+        return node;
     }
 };
 
@@ -111,10 +88,10 @@ int main()
     head = l2;
     head->next = new ListNode(6);
     head = head->next;
-    head->next = new ListNode(9);
+    head->next = new ListNode(4);
     head = head->next;
-    head->next = new ListNode(9);
-    head = head->next;
+    // head->next = new ListNode(9);
+    // head = head->next;
 
     Solution obj;
     ListNode* nodeList = obj.addTwoNumbers(l1, l2);
