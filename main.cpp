4170bc6e929533920f6e9eb87e7231557406e164
SeungHwan <seunghwanchoi@outlook.com>
2020-02-27 17:17:09 -0700

    refreash, variable name change for better understanding

--- main.cpp
+++ main.cpp
@@ -7,9 +7,71 @@
 
 using namespace std;
 
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void preOrderRecursive(TreeNode* node)
+{
+    if(!node)
+        return;
+
+    cout << node->val << " ";
+    preOrderRecursive(node->left);
+    preOrderRecursive(node->right);
+}
+
+
+class Solution {
+public:
+    TreeNode* helper(vector<int>& preorder, vector<int>& inorder,
+                     int preorderStart, int preorderEnd,
+                     int inorderStart, int inorderEnd)
+    {
+        if(inorderStart > inorderEnd || preorderStart > preorderEnd)
+            return NULL;
+
+        int headValue = preorder[preorderStart];
+        TreeNode* node = new TreeNode(headValue);
+        int mid = 0;
+        for(int i = inorderStart; i <= inorderEnd; i++)
+        {
+            if(inorder[i] == headValue)
+            {
+                mid = i;
+                break;
+            }
+        }
+
+        node->left = helper(preorder, inorder,
+                            preorderStart + 1, preorderStart + mid - inorderStart, inorderStart, mid - 1
+                           );
+
+        node->right = helper(preorder, inorder,
+                             preorderStart + mid - inorderStart + 1, preorderEnd,
+                             mid + 1, inorderEnd
+                            );
+        return node;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return helper(preorder, inorder, 0, preorder.size()-1, 0, inorder.size()-1);
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    Solution obj;
+    vector<int> preorder = {3,9,20,15,7};
+    vector<int> inorder = {9,3,15,20,7};
+
+    TreeNode* node = obj.buildTree(preorder, inorder);
+
+    preOrderRecursive(node);
 
     return 0;
 }
