a957e439434908c898ccf17a44e67275a8c90965
SeungHwan <seunghwanchoi@outlook.com>
2020-02-28 13:02:33 -0700

    added description

--- main.cpp
+++ main.cpp
@@ -8,19 +8,6 @@
 
 using namespace std;
 
-/*
-Letter Combinations of a Phone Number
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
-
-A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
-Example:
-Input: "23"
-Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-
-Note:
-Although the above answer is in lexicographical order, your answer could be in any order you want.
- */
-
 class Solution {
 public:
 
@@ -36,7 +23,6 @@ public:
         {"8", "tuv"},
         {"9", "wxyz"}
     };
-
 
     void getCombination(string comb, string digits)
     {
@@ -54,7 +40,7 @@ public:
                 getCombination(temp, d);
             }
         }
-    }
+    }//getCombination
 
 
     vector<string> letterCombinations(string digits) {
@@ -63,8 +49,7 @@ public:
 
         getCombination("", digits);
         return answer;
-
-    }//EOF
+    }//EOF letterCombinations
 };
 
 int main()
