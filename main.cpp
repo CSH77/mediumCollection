ec97237bc79b526aa61b17cfa1196f148dd7a8c0
SeungHwan <seunghwanchoi@outlook.com>
2020-02-28 15:04:13 -0700

    added description

--- main.cpp
+++ main.cpp
@@ -3,19 +3,6 @@
 #include <set>
 using namespace std;
 
-/*
-Input: [1,2,3]
-Output:
-[
-  [1,2,3],
-  [1,3,2],
-  [2,1,3],
-  [2,3,1],
-  [3,1,2],
-  [3,2,1]
-]
- */
-
 class Solution {
 public:
     vector<vector<int>> answer;
@@ -25,22 +12,18 @@ public:
         if(l == r)
         {
             vector<int> temp;
-            for(int n : num)
+            for(auto n : num)
                 temp.push_back(n);
+
             answer.push_back(temp);
             return;
         }
 
         for(int i = l; i < r; i++)
         {
-            //swap
-            swap(num[i], num[l]);
-
-            //call recusively,
+            swap(num[i], num[l] );
             permuHelper(num, l + 1, r);
-
-            //swapback.
-            swap(num[i], num[l]);
+            swap(num[i], num[l] ); //swap back
         }
     }//EOF
 
