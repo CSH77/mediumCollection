d57495c5fda057d5a00997cd05a0c16d983c48f3
SeungHwan <seunghwanchoi@outlook.com>
2020-02-29 21:09:56 -0700

    coin change.. still don't understand..

--- main.cpp
+++ main.cpp
@@ -7,23 +7,21 @@ using namespace std;
 
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount)
-    {
-        int dp[amount+1];
+    int coinChange(vector<int>& coins, int amount) {
 
-        for(int i = 0; i <= amount; i++)
-            dp[i] = 1 << 30;
+        // vector<int> dp = vector<int>(amount + 1, amount + 1);
+        vector<int> dp = vector<int>(amount + 1, amount + 1);
+        dp[0] = 0;
 
-        dp[0] = 0; // you need no coins to make 0
-        for(int i = 1; i <= amount; i++)
+        for (int i = 1; i <= amount; i++)
         {
-            for(int c : coins)
+            for (int j = 0; j < coins.size(); ++j)
             {
-                if(i >= c)
-                    dp[i] = min(dp[i], 1 + dp[i-c]);
+                if (coins[j] <= i)
+                    dp[i] = std::min(dp[i], dp[i - coins[j]] + 1);
             }
         }
-        return (dp[amount] == 1 << 30) ? -1: dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 };
 
