ca52db52cedfa21f31e934ebf4f39ba54d43f86f
SeungHwan <seunghwanchoi@outlook.com>
2020-03-01 01:09:27 -0700

    dynamic programming coin change, my version

--- main.cpp
+++ main.cpp
@@ -9,31 +9,30 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
 
-        // vector<int> dp = vector<int>(amount + 1, amount + 1);
-        vector<int> dp = vector<int>(amount + 1, amount + 1);
-        dp[0] = 0;
+        vector<int> amountVec(amount + 1, amount + 1);
+        amountVec[0] = 0;
 
-        for (int i = 1; i <= amount; i++)
+        for(int i = 1; i <= amount; i++)
         {
-            for (int j = 0; j < coins.size(); ++j)
+            for(int j = 0; j < coins.size(); j++)
             {
-                if (coins[j] <= i)
-                    dp[i] = std::min(dp[i], dp[i - coins[j]] + 1);
+                if(coins[j] <= i)
+                    amountVec[i] = min(amountVec[i], amountVec[i - coins[j]] + 1);
             }
         }
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        return amountVec[amount] == amount + 1 ? -1 : amountVec[amount];
     }
 };
 
 int main()
 {
-    vector<int> input = {1, 2, 5}; //amount 11, expect 3
-    // vector<int> input = {2}; //amount 3, expect -1
+    // vector<int> input = {1, 2, 5}; //amount 11, expect 3
+    vector<int> input = {2}; //amount 3, expect -1
     // vector<int> input = {2,5,10,1}; //amount 27, expect 4
     // vector<int> input = {186,419,83,408}; //amount 6249, expect 20
 
     Solution obj;
-    cout << obj.coinChange(input, 11);
+    cout << obj.coinChange(input, 3);
     // cout << obj.coinChange(input, 27);
     // cout << obj.coinChange(input, 6249);// expect 20
 
