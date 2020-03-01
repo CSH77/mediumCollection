c9c9bafeefd17e793edd8098a24e03575e9c87d8
SeungHwan <seunghwanchoi@outlook.com>
2020-02-29 17:14:47 -0700

    refresh sort method

--- main.cpp
+++ main.cpp
@@ -11,53 +11,42 @@ using namespace std;
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-
         if(intervals.empty())
             return 0;
 
-        //sort by first element
-        typedef function<bool(vector<int>,vector<int> )> Comperator;
-        Comperator comp = [](vector<int> elem1, vector<int> elem2)
-        {
-            return elem1[0] < elem2[0];
+        //sort by first
+        auto comp = [](vector<int>& a, vector<int>& b){
+            return a[0] < b[0];
         };
         sort(intervals.begin(), intervals.end(), comp);
 
-        //use priority queue keep update end time.
+        //use priority_que que holds end time of each room, check if starting time is conflict with end time
         priority_queue<int, vector<int>, greater<int>> pque;
-        pque.push(intervals[0][1]);
+        pque.push(intervals[0][1]); //push end time
 
         for(int i = 1; i < intervals.size(); i++)
         {
-            int current = pque.top();
-            if(current <= intervals[i][0]) //previous meeting is done, free to use meeting room now.
+            int roomOccupiedEndTime = pque.top();
+            int roomToReserveStartTime = intervals[i][0];
+            if(roomOccupiedEndTime <= roomToReserveStartTime) //now room is free to use
             {
                 pque.pop();
-                pque.push(intervals[i][1]);
+                pque.push(intervals[i][1]); //reseve with end time
             }
-            else //meeting room is still being used, need another meeting room.
+            else  //room is not free yet, need another room
             {
                 pque.push(intervals[i][1]);
             }
         }
-
         return pque.size();
-
-        // for(vector<int> v : intervals)
-        // {
-        //     for(int n : v)
-        //         cout << n << ":";
-        //     cout << "    ";
-        // }
-        // cout << endl;
     }
 };
 
 int main()
 {
-    // vector<vector<int>> input = {{0, 30},{5, 10},{15, 20}}; //expect 2
+    vector<vector<int>> input = {{0, 30},{5, 10},{15, 20}}; //expect 2
     // vector<vector<int>> input = {{7,10},{2,4}}; //expect 1
-    vector<vector<int>> input = {{13,15},{1,13}}; //expect 1
+    // vector<vector<int>> input = {{13,15},{1,13}}; //expect 1
 
     Solution obj;
     cout << obj.minMeetingRooms(input);
