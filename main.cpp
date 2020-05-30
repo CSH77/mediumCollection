e7d51f8a73c150c67a1a81abfc8a38d06fb14b24
SeungHwan <seunghwanchoi@outlook.com>
2020-05-30 01:33:03 -0600

    working solution... hmm

--- main.cpp
+++ main.cpp
@@ -10,43 +10,24 @@ using namespace std;
 
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        if(nums.size() == 1)
+    int subarraySum(vector<int>& nums, int k)
+    {
+        int n=nums.size();
+        int ans=0;
+        int cur_sum=0;
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++)
         {
-            if(nums[0] == k)
-                return 1;
-            else
-                return 0;
+            cur_sum += nums[i];
+            if(cur_sum == k)
+                ans++;
+
+            if(m.find(cur_sum - k) != m.end())
+                ans += m[cur_sum - k];
+
+            m[cur_sum]++;
         }
-        else if(nums.size() < 1)
-            return 0;
-
-        unordered_map<int, int> umap;
-
-        umap.insert(make_pair(0, 1));
-
-        int sum = 0;
-        int sum_minus_k = sum - k;
-        int count = 0;
-
-        for(int i = 0; i < nums.size(); i++)
-        {
-            //add number to lastMap value and find that value as key.
-            sum += nums[i];
-            sum_minus_k += nums[i];
-            auto search = umap.find(sum);
-            if(search != umap.end()) //number exist, add occurance,
-                search->second += 1;
-            else //number does not exist, add new pair of number.
-                umap.insert(make_pair(sum, 1));
-
-            //if sum-k as key exist, increase count;
-            search = umap.find(sum_minus_k);
-            if(search != umap.end()) //exist
-                count++;
-        }
-
-        return count;
+        return ans;
     }
 };
 
