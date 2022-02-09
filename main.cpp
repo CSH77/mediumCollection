5401c61355db4c293e0644ea20d502f921bd77ca
SeungHwan <seunghwanchoi@outlook.com>
2022-02-08 20:45:07 -0800

    sliding window better description

--- main.cpp
+++ main.cpp
@@ -9,32 +9,42 @@ public:
     int lengthOfLongestSubstring(string s)
     {
         set<char> myset;
-        int n = s.length();
+        int stringSize = s.length();
         int ans = 0, i = 0, j = 0;
 
-        while(i < n && j < n)
+        while(i < stringSize && j < stringSize)
         {
             auto search = myset.find(s.at(j));
-            if(search == myset.end())
+            if(search == myset.end()) //The char not used,
             {
-                myset.insert(s.at(j++));
+                //add new char to database
+                myset.insert(s.at(j));
+
+                //update index
+                j++;
+
+                //refresh answer.
                 ans = max(ans, j - i);
             }
-            else
+            else //new char used already.
             {
-                myset.erase(s.at(i++));
+                //remove first char from the database.
+                myset.erase(s.at(i));
+
+                //update index
+                i++;
+
             }
         }
-
         return ans;
     }
 
 };
 int main()
 {
-    //string input = "pwwkew"; //expect The answer is "wke", with the length of 3.
-    //string input = "abcabcbb";//  The answer is "abc", with the length of 3.
-    string input = "dvdf"; //expect 3
+    string input = "pwwkew"; //expect The answer is "wke", with the length of 3.
+    // string input = "abcabcbb";//  The answer is "abc", with the length of 3.
+    // string input = "dvdf"; //expect 3
     Solution obj;
     cout << obj.lengthOfLongestSubstring(input);
 
