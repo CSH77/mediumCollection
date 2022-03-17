dd05a891545885e0cf07235d943c2b0729926b23
SeungHwan <seunghwanchoi@outlook.com>
2022-03-17 04:14:15 -0600

    point method

--- main.cpp
+++ main.cpp
@@ -8,18 +8,25 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int cur = 0;
-        int left =0;
-        int right = nums.size() - 1;
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = nums.size() - 1;
 
-        while(cur <= right)
+        while(p2 <= p3)
         {
-            if(nums[cur] == 0)
-                swap(nums[cur++], nums[left++]);
-            else if(nums[cur] == 2)
-                swap(nums[cur], nums[right--]);
+            if(nums[p2] == 0)
+            {
+                swap(nums[p1], nums[p2]);
+                p1++;
+                p2++;
+            }
+            else if(nums[p2] == 2)
+            {
+                swap(nums[p2], nums[p3]);
+                p3--;
+            }
             else
-                cur++;
+                p2++;
         }
     }//EOF
 };
@@ -29,6 +36,7 @@ int main()
     Solution obj;
 
     vector<int> input = {2,0,2,1,1,0};
+    //vector<int> input = {2,0,1};
     obj.sortColors(input);
 
     for(int n : input)
