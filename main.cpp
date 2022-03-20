b57e4d8eef6ecbeef06c75f3f215bb9b1f67fa6b
SeungHwan <seunghwanchoi@outlook.com>
2022-03-19 22:53:09 -0700

    decide mid in while loop

--- main.cpp
+++ main.cpp
@@ -10,15 +10,16 @@ class Solution {
 public:
 
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        int mid = (start + end) / 2;
-
         if(nums.size() == 0)
             return -1;
 
+        int start = 0;
+        int end = nums.size() - 1;
+        // int mid = (start + end) / 2;
+
         while(start < end)
         {
+            int mid = (start + end) / 2;
             if(nums[mid] == target)
                 return mid;
 
@@ -29,11 +30,11 @@ public:
                 else  //not in range. go right
                     start = mid + 1;
             }
-            else
+            else //it is rotated at some point
             {
-                if(nums[mid] < target && nums[end] >= target)
+                if(nums[mid] < target && nums[end] >= target)  //in range go right
                     start = mid + 1;
-                else
+                else //go left
                     end = mid - 1;
             }
             mid = (start + end) / 2;
