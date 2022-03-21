7a097d9a38633f37b4d71abf87509d97f4275c42
SeungHwan <seunghwanchoi@outlook.com>
2022-03-20 22:56:41 -0700

    search number x row and y

--- main.cpp
+++ main.cpp
@@ -10,25 +10,37 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size() - 1;;
-        int col = 0;
 
-        while(row >= 0 && col < matrix[0].size())
+        int maxX = matrix[0].size();
+        int maxY = matrix.size();
+
+        int x = 0, y = 0;
+
+        while(x < maxX)
         {
-            int current = matrix[row][col];
-
-            if(current == target)
+            if(matrix[y][x] == target)
                 return true;
 
-            if(current > target) //find row first
-            {
-                row--;
-            }
-            else //now current is smaller than target, iterate coloumn means increasing target value
-            {
-                col++;
-            }
+            if(matrix[y][x] > target)
+                break;
+
+            x++;
         }
+        if(x >= maxX)
+            x = x - 1;
+
+        while(x < maxX && y < maxY && x >= 0 && y >= 0)
+        {
+            int currentValue = matrix[y][x];
+            if(currentValue == target)
+                return true;
+
+            if(currentValue > target)
+                x = x - 1;
+            else
+                y = y + 1;
+        }
+
         return false;
     }
 };
@@ -43,9 +55,13 @@ int main()
                                     {18, 21, 23, 26, 30}
                                 };
 
+    // vector<vector<int>> input = { {5}, {6} }; //true
+
     Solution obj;
     // cout << obj.searchMatrix(input, 5) << endl; //expect true;
-    cout << obj.searchMatrix(input, 25) << endl; //expect false;
+    // cout << obj.searchMatrix(input, 25) << endl; //expect false;
+
+    cout << obj.searchMatrix(input, 5) << endl; //expect true;
 
     return 0;
 }
