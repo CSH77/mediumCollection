9807b70b7dde91d8d663b8ff9a9a96a3bb329556
SeungHwan <seunghwanchoi@outlook.com>
2022-03-23 02:48:35 -0600

    check from rear

--- main.cpp
+++ main.cpp
@@ -6,19 +6,18 @@ using namespace std;
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        if(nums.size() < 2)
+            return true;
 
         int goalIndex = nums.size() - 1;
-        int currentIndex = goalIndex;
-
+        int currentIndex = 0;
         for(int i = nums.size() - 1; i >= 0; i--)
         {
+            int maxJump = nums[i];
             currentIndex = i;
-            if( (nums[currentIndex] + currentIndex ) >= goalIndex )
-            {
+            if( (maxJump + currentIndex) >= goalIndex  )
                 goalIndex = currentIndex;
-            }
         }
-
         if(goalIndex == 0)
             return true;
         else
@@ -29,8 +28,8 @@ public:
 
 int main()
 {
-    // vector<int> input = {2,3,1,1,4}; //true
-    vector<int> input = {3,2,1,0,4}; //true
+    vector<int> input = {2,3,1,1,4}; //true
+    //vector<int> input = {3,2,1,0,4}; //false
 
     Solution obj;
 
