cf016885c12ffc99c372246655ffb5a2d7ea74e1
SeungHwan <seunghwanchoi@outlook.com>
2022-03-26 02:11:00 -0700

    uses binary search

--- main.cpp
+++ main.cpp
@@ -7,42 +7,50 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s)
-    {
-        set<char> myset;
-        string currentStr = "";
-        int maxLength = 0;
-        for(int i = 0; i < s.length(); i++)
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        sub.push_back(nums[0]);
+
+        for(int i = 1; i < nums.size(); i++)
         {
-            auto search = myset.find(s.at(i));
-            if(search != myset.end())
-            {
-                size_t pos = currentStr.find( s.at(i) );
-                currentStr = currentStr.substr(pos + 1);
-                currentStr = currentStr + s.at(i);
-            }
+            // int test = sub[sub.size() - 1];
+            int currentValue = nums[i];
+            if(currentValue > sub.back())
+                sub.push_back(currentValue);
             else
             {
-                currentStr = currentStr + s.at(i);
-                myset.insert(s.at(i));
+                int index = binarySearch(sub, currentValue);
+                sub[index] = currentValue;
             }
-
-            maxLength = max(maxLength, int(currentStr.length()) );
-        }  
-
-        
-
-        return maxLength;
+        }
+        return sub.size();
     }
 
+    int binarySearch(vector<int>& sub, int target)
+    {
+        int left = 0;
+        int right = sub.size() - 1;
+        while(left < right)
+        {
+            int mid = (left + right) / 2;
+            if(sub[mid] == target)
+                return mid;
+            else if(sub[mid] < target) //go right
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
 };
+
+
 int main()
 {
-    // string input = "pwwkew"; //expect The answer is "wke", with the length of 3.
-    // string input = "abcabcbb";//  The answer is "abc", with the length of 3.
-    string input = "dvdf"; //expect 3
     Solution obj;
-    cout << obj.lengthOfLongestSubstring(input);
+    vector<int> input = {10,9,2,5,3,7,101,18}; //expect 4
+
+    cout << obj.lengthOfLIS(input);
 
     return 0;
 }
