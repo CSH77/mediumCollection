a0be860d40c950722222e3d79e20c6da477b6231
SeungHwan <seunghwanchoi@outlook.com>
2022-03-27 17:39:40 -0700

    count 5 divisible times to find out count of zeros

--- main.cpp
+++ main.cpp
@@ -7,34 +7,17 @@ using namespace std;
 class Solution {
 public:
     int trailingZeroes(int n) {
-        vector<size_t> dp(n + 1, 0);
-        if(n <= 2)
-            return 0;
-
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i < n + 1; i++)
+        int zeroCount = 0;
+        for(int i = 5; i < n + 1; i += 5)
         {
-            dp[i] = i * dp[i - 1];
+            int current = i;
+            while(current % 5 == 0)
+            {
+                zeroCount++;
+                current = current / 5;
+            }
         }
-
-        //find trailing zero
-        size_t total = dp[n];
-        int count = 0;
-        cout << "total: " << total << endl;
-
-        while(total)
-        {
-            if ( total % 10 == 0 )
-                count++;
-            else
-                return count;
-
-            total = total / 10;
-        }
-        return count;
+        return zeroCount;
     }
 };
 
@@ -42,9 +25,9 @@ int main()
 {
     Solution obj;
     //cout << obj.trailingZeroes(5); //120 => 1 trailing zero.
-    // cout << obj.trailingZeroes(4);
+     //cout << obj.trailingZeroes(4);
     //cout << obj.trailingZeroes(3); //3! = 6, no trailing zero.
-    cout << obj.trailingZeroes(30); //expect 1;
+    cout << obj.trailingZeroes(30); //expect 7;
 
     return 0;
 }
