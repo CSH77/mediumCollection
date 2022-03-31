73bc720555ac504504572273e2878069a41da5e7
SeungHwan <seunghwanchoi@outlook.com>
2022-03-30 23:14:00 -0700

    brute force solution

--- main.cpp
+++ main.cpp
@@ -6,6 +6,35 @@
 #include <stack>
 
 using namespace std;
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int numberOfPeople;
+    int findCelebrity(int n) {
+        numberOfPeople = n;
+        for(int i = 0; i < n; i++){
+            if( isCelebrity(i))
+                return i;
+        }
+        return -1;
+    }
+
+    bool isCelebrity(int i)
+    {
+        for(int j = 0; j < numberOfPeople; j++)
+        {
+            if(i == j) //know themselves, skip.
+                continue;
+
+            if( knows(i,j) || !knows(j,i))
+                return false;
+        }
+        return true;
+    }
+};
+
 
 int main()
 {
