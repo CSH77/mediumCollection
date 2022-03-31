bcb9f017112b494dd78092d9ec5ca935c441a13d
SeungHwan <seunghwanchoi@outlook.com>
2022-03-30 18:13:47 -0700

    first solution

--- main.cpp
+++ main.cpp
@@ -7,9 +7,40 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        auto comp = [](pair<int,int>& a, pair<int,int>& b){
+            return a.second < b.second;
+        };
+
+        map<int, int> mymap;
+        for(auto item : nums)
+        {
+            auto search = mymap.find(item);
+            if(search != mymap.end()) //exist, add count
+                search->second++;
+            else //add new
+                mymap.insert(make_pair(item, 1));
+        }
+
+        //find most frequent Element by sorting.
+        //sort map by 2nd element.
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> pque(comp);
+
+        for(auto item : mymap)
+            pque.push(make_pair(item.first, item.second));
+
+        return pque.top().first;
+
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    Solution obj;
+    vector<int> input = {2,2,1,1,1,2,2};
+    cout << obj.majorityElement(input);
 
     return 0;
 }
