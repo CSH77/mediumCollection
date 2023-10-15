0d5c38f96a507f96756d71a1673e225d63c87c22
SeungHwan <seunghwanchoi@outlook.com>
2023-10-15 01:42:59 -0700

    sort by value

--- main.cpp
+++ main.cpp
@@ -9,7 +9,22 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world!" << endl;
+    auto comp = [](pair<int,string>& a, pair<int,string>& b){
+            return a.second > b.second;
+    };
+
+    priority_queue<pair<int,string>, vector<pair<int,string>>, decltype(comp)> pque(comp);
+
+    pque.push(make_pair(1, "f"));
+    pque.push(make_pair(2, "c"));
+    pque.push(make_pair(3, "d"));
+
+    while(!pque.empty())
+    {
+        auto item = pque.top();
+        cout << item.first << ":" << item.second << endl;
+        pque.pop();
+    }
 
     return 0;
 }
