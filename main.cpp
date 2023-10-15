5d92edb5646a11d38a08f4637d70bc2d9b55e733
SeungHwan <seunghwanchoi@outlook.com>
2023-10-15 01:27:13 -0700

    multimap examples print out indivisual key values

--- main.cpp
+++ main.cpp
@@ -9,7 +9,31 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world!" << endl;
+    multimap<int,int> mmap;
+    mmap.insert(make_pair(1,1));
+    mmap.insert(make_pair(1,2));
+    mmap.insert(make_pair(1,3));
+    mmap.insert(make_pair(3,1));
+    mmap.insert(make_pair(3,2));
+    mmap.insert(make_pair(2,1));
+    mmap.insert(make_pair(2,2));
+
+    // for(auto item : mmap)
+    //     cout << item.first << "::" << item.second << endl;
+
+    for(auto iter = mmap.begin(); iter != mmap.end(); iter = mmap.upper_bound(iter->first) )
+    {
+        auto iter2 = mmap.equal_range(iter->first);
+        cout << "key: " << iter->first << " ";
+        cout << "values: ";
+        for(auto it = iter2.first; it != iter2.second; it++)
+        {
+            cout << it->second << " ";
+        }
+        cout << endl;
+    }
+
+
 
     return 0;
 }
