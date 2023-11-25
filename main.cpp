b73d474034d5df167fbfbe5adb49874c23fd590b
SeungHwan <seunghwanchoi@outlook.com>
2023-11-24 19:36:43 -0800

    initial commit

--- main.cpp
+++ main.cpp
@@ -4,12 +4,63 @@
 #include <map>
 #include <queue>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 
+
+struct Node
+{
+    Node* next;
+    int val;
+
+    Node(int v, Node* n = nullptr) : val(v), next(n){}
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    Node* virHead = new Node(0);
+    Node* cur = virHead;
+    cur->next = new Node(1);
+    cur = cur->next;
+
+    cur->next = new Node(2);
+    cur = cur->next;
+
+    cur->next = new Node(3);
+    cur = cur->next;
+
+    cur = virHead->next;
+    while(cur)
+    {
+        cout << cur->val << ", ";
+        cur = cur->next;
+    }
+    cout << endl;
+
+    //reverse linked list.
+    cur = virHead->next;
+    Node* tail = cur;
+    Node* prev = virHead;
+    Node* next = cur;
+
+    while(cur)
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    tail->next = nullptr;
+    cur = prev;
+
+    cout << "reverse linkedList" << endl;
+    while(cur)
+    {
+        cout << cur->val << ", ";
+        cur = cur->next;
+    }
+    cout << endl;
 
     return 0;
 }
